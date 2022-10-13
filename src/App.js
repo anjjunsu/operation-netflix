0ea82304c91750dbc69d1801c4a76b49ae844c21
Junsu An <anjjunsu@gmail.com>
2022-10-12 21:42:35 -0700

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import './App.css';
-import React, { useState } from 'react';
-
 function App() {
-  const [minutes, setMinutes] = useState();
-
-  const onChange = (event) => {
-    setMinutes(event.target.value);
-  };
-
-  return (
-    <div>
-        <h1>Converter</h1>
-        <label htmlFor="mins">Minutes</label>
-        <input 
-            id="mins" 
-            value={minutes}
-            placeholder="Minutes" 
-            type="number"
-            onChange={onChange}/>
-        <span>Your minutes: {minutes}</span>
-        <label htmlFor="hrs">Hours</label>
-        <input id="hrs" placeholder="Hours" type="number"/>
-    </div>
-  );
+  return <h1>Hello</h1>;
 }
 
 export default App;
-
